71a7e7fa724c56a7e9cb34e5a05484bb0726912f
Rahul Maurya <rahulmaurya.mnnit@gmail.com>
2020-05-18 12:39:30 -0400

    updated comments in question_1

--- question_1/solution.py
+++ question_1/solution.py
@@ -5,25 +5,29 @@ import sys
 
 def find_longest_word_in_string(letters, words):
     letter_positions = {}
+    # preprocessing the big word  : every letter with their position in the word
     for index, letter in enumerate(letters):
         if letter not in letter_positions:
             letter_positions[letter] = []
         letter_positions[letter].append(index)
+    # sort the dictionary in decsending order of their length
     for word in sorted(words, key=lambda w: len(w), reverse=True):
-        head = 0
+        position = 0
+        # position of letter where next letter can be found
         flag = False
+        # keeping the flag if dictionary word fail to be a subsequence; True - failed to be subsequence, False - might be subsequence
         for letter in word:
             next_position = []
             if letter not in letter_positions:
                 flag = True
                 break
             for p in letter_positions[letter]:
-                if p >= head:
+                if p >= position:
                     next_position.append(p)
             if len(next_position) == 0:
                 flag = True
                 break
-            head = next_position[0] + 1
+            position = next_position[0] + 1
 
         if not flag:
             return word
